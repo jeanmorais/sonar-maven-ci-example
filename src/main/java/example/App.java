2d2eadce4cdf9462de1066650103662937f89420
Jean Morais <jeancdemorais@gmail.com>
2018-06-28 21:09:09 -0300

    Initial commit

---
+++ src/main/java/example/App.java
@@ -0,0 +1,12 @@
+package example;
+
+public class App 
+{
+	public void coveredByUnitTest() {
+		System.out.println("coveredByUnitTest");
+	}
+
+	public void notCovered() {
+		System.out.println("notCovered");
+	}
+}
---
+++ src/test/java/example/AppTest.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void test() {
+		new App().coveredByUnitTest();
+	}
+}
